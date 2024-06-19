49c0b6e32b9517c737a53c312d0fbe24f67d5b27
Gaurav Khatri <105037911+gauravkhatriweb@users.noreply.github.com>
2024-06-19 16:43:04 +0500

    1.2 Version Program.cs

--- hypotenuse-calculator-program/Program.cs
+++ hypotenuse-calculator-program/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace hypotenuse_calculator_program
 {
@@ -11,14 +6,26 @@ namespace hypotenuse_calculator_program
     {
         static void Main(string[] args)
         {
+            // Prompt the user to enter side A
             Console.WriteLine("Enter side A:");
+            // Read and convert side A input to a double
             double a = Convert.ToDouble(Console.ReadLine());
+            
+            // Prompt the user to enter side B
             Console.WriteLine("Enter side B:");
+            // Read and convert side B input to a double
             double b = Convert.ToDouble(Console.ReadLine());
 
-
+            // Calculate the hypotenuse using the Pythagorean theorem
             double c = Math.Sqrt((a * a) + (b * b));
-            Console.WriteLine("This Hypotenuse is: " + c);
+            
+            // Output the calculated hypotenuse
+            Console.WriteLine("The Hypotenuse is: " + c);
+
+            // Watermark
+            Console.WriteLine("Made By Gaurav Khatri love from Pakistan");
+            
+            // Wait for user input to close the console window (optional)
             Console.ReadKey();
         }
     }
